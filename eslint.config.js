630ed5f7446021c56936fbbf789a868edefab0af
Jessica Galvis <jessicagalvis@Jessicas-MacBook-Pro.local>
2025-11-22 09:30:23 -0500

    Adding dev and prod dependencies, configuring precommit hook, configuring prettier, eslint, jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,77 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import jestPlugin from 'eslint-plugin-jest';
+import testingLibrary from 'eslint-plugin-testing-library';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        process: 'readonly',
+        'import.meta': 'readonly',
+      },
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.recommended,
+      prettierConfig,
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'prettier/prettier': 'error',
     },
   },
-])
+  {
+    files: ['**/*.test.{ts,tsx}', 'src/setupTests.ts', '**/__tests__/**'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      jest: jestPlugin,
+      'testing-library': testingLibrary,
+    },
+    extends: [
+      jestPlugin.configs['flat/recommended'],
+      testingLibrary.configs['flat/react'],
+    ],
+    rules: {
+      'testing-library/no-unnecessary-act': 'warn',
+      'testing-library/prefer-screen-queries': 'error',
+    },
+  },
+]);
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  roots: ['<rootDir>/src'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+};
